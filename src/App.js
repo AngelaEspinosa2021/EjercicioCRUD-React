ecccf66099a1c421945348bff62b88ece26c4687
AngelaEspinosa <titagena@gmail.com>
2022-05-05 22:14:17 -0500

    Se crea la estructura basica de la tabla que mostrara los usuarios.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-
+import UserTable from './components/UserTable';
 
 function App() {
   return (
-    <div>Hola Mundo</div>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable />
+        </div>
+      </div>
+    </div>
   );
 }
 
